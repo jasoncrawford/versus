9611b72ad3d4db23e95be3cd4e5737b8c68cc193
Jason Crawford <jason@jasoncrawford.org>
2018-09-29 18:40:43 -0700

    Use request library instead of hand-rolled request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const https = require('https');
 const querystring = require('querystring');
+const request = require('request-promise-native');
 const htmlEntities = require('html-entities');
 
 let app = express();
@@ -11,27 +12,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-const get = url => {
-  return new Promise((resolve, reject) => {
-    let lib = url.startsWith('https') ? https : http;
-    let request = lib.get(url, response => {
-      if (response.statusCode < 200 || response.statusCode > 299) {
-         reject(new Error(`HTTP ${response.statusCode}`));
-       }
-
-      let body = [];
-      response.on('data', chunk => body.push(chunk));
-      response.on('end', () => resolve(body.join('')));
-    });
-
-    request.on('error', reject)
-  })
-};
-
 const fetchCompletions = async q => {
   let queryString = querystring.stringify({q, client: 'psy-ab'});
   let url = `https://www.google.com/complete/search?${queryString}`;
-  let body = await get(url);
+  let body = await request(url);
   let data = JSON.parse(body);
   return data[1].map(item => item[0]);
 }
